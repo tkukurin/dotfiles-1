ec4d8a564bb8a905726f85a713ef813d70d54336
ℳichiel ℛoos <michiel@michielroos.com>
2017-05-06 12:11:16 +0200

    Cleanup bootstrap Add dircolors

--- linux-bootstrap.sh
+++ linux-bootstrap.sh
@@ -8,70 +8,65 @@ else
 	home=$1
 fi
 
-# Install prerequisites
+echo "Installing powerline shell..."
 if [ ! -d $home/powerline-shell ]
 then
 	cd $home/${PWD##*/}
 	git submodule update --init powerline-shell
 	cd $home/${PWD##*/}/powerline-shell && ./install.py
 	cd ..
-	ln -s $home/${PWD##*/}/powerline-shell/powerline-shell.py $home/powerline-shell.py
+
+	if [ -e $home/powerline-shell.py ] || [ -L $home/powerline-shell.py ]; then
+	    mv $home/powerline-shell.py $home/powerline-shell.py.original
+    fi
+    ln -s $home/${PWD##*/}/powerline-shell/powerline-shell.py $home/powerline-shell.py
 fi
+
+echo "Installing zim..."
 if [ ! -d $home/.zim ]
 then
 	git clone --recursive https://github.com/Eriner/zim.git ${ZDOTDIR:-${HOME}}/.zim
 fi
 
-# Create symlinks
-if [ -e $home/.vim ] || [ -L $home/.vim ]
-then
-	mv $home/.vim $home/.vim.original
-fi
-if [ -e $home/.vimrc ] || [ -L $home/.vimrc ]
-then
-	mv $home/.vimrc $home/.vimrc.original
-fi
-if [ -e $home/.tmux.conf ] || [ -L $home/.tmux.conf ]
-then
-	mv $home/.tmux.conf $home/.tmux.conf.original
-fi
-if [ -e $home/.bashrc ] || [ -L $home/.bashrc ]
-then
-	mv $home/.bashrc $home/.bashrc.original
-fi
-if [ -e $home/.zshrc ] || [ -L $home/.zshrc ]
-then
-	mv $home/.zshrc $home/.zshrc.original
-fi
-if [ -e $home/.zimrc ] || [ -L $home/.zimrc ]
-then
-	mv $home/.zimrc $home/.zimrc.original
-fi
-if [ -e $home/tmux-launch.sh ] || [ -L $home/tmux-launch.sh ]
-then
-	mv $home/tmux-launch.sh $home/tmux-launch.sh.original
-fi
-if [ -e $home/.config/powerline ] || [ -L $home/.config/powerline ]
-then
-	mv $home/.config/powerline $home/.config/powerline.original
-fi
+echo "Creating symlinks..."
+itemsToLink=(
+    .bashrc
+    .config/powerline
+    .dircolors
+    tmux-launch.sh
+    .tmux.conf
+    .vim
+    .vimrc
+    .zimrc
+    .zshrc
+)
+
+length=${#itemsToLink[*]}
+i=0
+while [ $i -lt $length ];
+do
+    item=${itemsToLink[$i]}
+    if [ -e "$home/$item" ] || [ -L "$home/$item" ]; then
+        if [ -L "$home/$item.original" ]; then
+            rm "$home/$item.original"
+        fi
+    	mv "$home/$item" "$home/$item.original"
+        ln -s $home/${PWD##*/}/$item $home/$item
+    else
+        ln -s $home/${PWD##*/}/$item $home/$item
+    fi
+    let i++
+done
 
 mkdir -p $home/.config
-ln -s $home/${PWD##*/}/.vim $home/.vim
-ln -s $home/${PWD##*/}/.vimrc $home/.vimrc
-ln -s $home/${PWD##*/}/.tmux.conf $home/.tmux.conf
-ln -s $home/${PWD##*/}/.bashrc $home/.bashrc
-ln -s $home/${PWD##*/}/.zimrc $home/.zimrc
-ln -s $home/${PWD##*/}/.zshrc $home/.zshrc
-ln -s $home/${PWD##*/}/powerline-config $home/.config/powerline
-ln -s $home/${PWD##*/}/tmux-launch.sh $home/tmux-launch.sh
+
 chmod u+x $home/tmux-launch.sh
 
-# Initialize submodules
+echo "Initializing submodules..."
 git submodule update --init --recursive
 
-# Compile command-t for our ruby version
-cd $home/${PWD##*/}/.vim/bundle/Command-T/ruby/command-t
+echo "Compiling command-t for current ruby version..."
+cd $home/${PWD##*/}/.vim/plugged/command-t/ruby/command-t
 if command -v ruby > /dev/null 2>&1; then
 	ruby extconf.rb
 	if command -v make > /dev/null 2>&1; then
