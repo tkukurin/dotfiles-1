842fa845583fa979dd3595d7efdfda0a75f08651
ℳichiel ℛoos <michiel@michielroos.com>
2017-05-15 18:15:15 +0200

    Move scripts to bin dir

--- bin/update-modules.sh
+++ bin/update-modules.sh
@@ -7,10 +7,6 @@ git submodule foreach git fetch --all
 git submodule foreach git reset --hard origin/master
 cd .vim/bundle/powerline
 git reset --hard origin/develop
-cd ../../../powerline-shell
-git stash pop
-./install.py
-cd ../
 fc-cache -vf ./powerline-fonts
 echo Clearing VIM View Cache
 find ./.vim/view -type f -exec rm {} +
