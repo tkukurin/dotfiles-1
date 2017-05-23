a09dd3b13f42af9fe9a19e857e8c5e8f4901f040
ℳichiel ℛoos <michiel@michielroos.com>
2017-05-23 21:12:49 +0200

    zsh: Update TERMCAP colours

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -8,12 +8,12 @@ export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='38;5;202'
 
 export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_md=$'\E[01;38;5;65m'  # begin bold
 export LESS_TERMCAP_me=$'\E[0m'           # end mode
 export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
 export LESS_TERMCAP_so=$'\E[38;33;172m'   # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-export LESS_TERMCAP_us=$'\E[04;38;5;172m' # begin underline
+export LESS_TERMCAP_us=$'\E[04;38;5;67m' # begin underline
 export LESS=-r
 
 [[ -z $TMUX ]] && export TERM="xterm-256color"
