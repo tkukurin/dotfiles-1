b1d6851eef770cdf9e8a53fc20ef0ff602f33611
ℳichiel ℛoos <michiel@michielroos.com>
2017-05-23 22:12:05 +0200

    mc: Add midnight comander configuration

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -11,12 +11,15 @@ export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
 export LESS_TERMCAP_md=$'\E[01;38;5;67m'  # begin bold
 export LESS_TERMCAP_me=$'\E[0m'           # end mode
 export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-export LESS_TERMCAP_so=$'\E[38;33;65m'   # begin standout-mode - info box
+export LESS_TERMCAP_so=$'\E[38;33;65m'    # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
 export LESS_TERMCAP_us=$'\E[04;38;5;172m' # begin underline
 export LESS=-r
 
 [[ -z $TMUX ]] && export TERM="xterm-256color"
+
+# Midnight commander wants this:
+export COLORTERM=truecolor
 
 # Set GPG TTY
 export GPG_TTY=$(tty)
