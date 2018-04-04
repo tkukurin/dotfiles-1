d3c6ee40fdd2606c04384b3472248c1eed4e9805
Michiel Roos <michiel@michielroos.com>
2018-04-04 14:28:20 +0200

    i3: fix keyring startup script

--- .config/i3/scripts/gnome-keyring.sh
+++ .config/i3/scripts/gnome-keyring.sh
@@ -1,3 +1,3 @@
-!#/bin/sh
+#!/usr/bin/sh
 eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
-export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
+export SSH_AUTH_SOCK
