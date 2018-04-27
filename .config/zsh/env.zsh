bdeae1115fe8e8baecac0fdd3c0d4b88f7a9625e
Michiel Roos <michiel@michielroos.com>
2018-04-27 09:10:51 +0200

    zsh: fix nvm dir

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -39,10 +39,10 @@ export GPG_TTY=$(tty)
 
 # Node version manager
 export NVM_DIR="$HOME/.nvm"
-[[ -s "/usr/local/opt/nvm/nvm.sh" ]] && . "/usr/local/opt/nvm/nvm.sh"
+[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
 
 if (( $+commands[luarocks] )); then
-	eval `luarocks path --bin`
+    eval `luarocks path --bin`
 fi
 
 path=(\
