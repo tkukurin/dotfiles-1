00724735b1f8ef92d3b825a7c75ea3b71cd7e5f1
Michiel Roos <michiel@michielroos.com>
2018-07-11 13:49:00 +0200

    awesome: Disable compton, map xinput to actual device name

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -2,26 +2,21 @@
 local awful = require("awful")
 autorun = true
 autorunApps = {
-    "systemctl --user start awesome.target",
-    "xrandr --output eDP1 --mode 1920x1080 --pos 1920x0 --output HDMI1 --primary --mode 1920x1080 --pos 0x0",
-    "compton -b",
-    "pactray",
-    "/usr/lib/gsd-xsettings",
-    "dex -a -e awesome",
-    "udiskie",
+   "systemctl --user start awesome.target",
+   "xrandr --output HDMI-1 --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off",
+   -- "compton -b",
+   "pactray",
+   "/usr/lib/gsd-xsettings",
+   "dex -a -e awesome",
+   "udiskie",
 
     -- Switch Wacom pen buttons; click button is secondary
     "xinput set-prop 'Wacom Intuos PT M Pen stylus' 293 1572865",
     "xinput set-prop 'Wacom Intuos PT M Pen stylus' 294 1572867",
 
-    -- Touchpads: tap to click
-    "xinput set-prop 'ETPS/2 Elantech Touchpad' 278 1",
-    -- Touchpads: natural scrolling
-    "xinput set-prop 'ETPS/2 Elantech Touchpad' 286 1",
-    -- Touchpads: Tapping drag
-    "xinput set-prop 'ETPS/2 Elantech Touchpad' 280 0",
-    -- Touchpads: disable touchpad while typing
-    "xinput set-prop 'ETPS/2 Elantech Touchpad' 288 1",
+    "xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Tapping Enabled' 1",
+    "xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Natural Scrolling Enabled' 1",
+    "xinput set-prop 'ETPS/2 Elantech Touchpad' 'libinput Disable While Typing Enabled' 1",
 
     -- Lock screen and suspend
     "xss-lock -- betterlockscreen -s dim",
