658c11c7fe0a06334e47500537ff776f23b9a3a5
Michiel Roos <michiel@michielroos.com>
2020-06-22 19:27:48 +0200

    Zsh: keybindings: fix history search

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -1,14 +1,24 @@
+# key=(
+#     BackSpace  "${terminfo[kbs]}"
+#     Home       "${terminfo[khome]}"
+#     End        "${terminfo[kend]}"
+#     Insert     "${terminfo[kich1]}"
+#     Delete     "${terminfo[kdch1]}"
+#     Up         "${terminfo[kcuu1]}"
+#     Down       "${terminfo[kcud1]}"
+#     Left       "${terminfo[kcub1]}"
+#     Right      "${terminfo[kcuf1]}"
+#     PageUp     "${terminfo[kpp]}"
+#     PageDown   "${terminfo[knp]}"
+# )
+
 bindkey '^w' backward-kill-word
 bindkey '^h' backward-delete-char
-autoload -Uz history-search-end
-zle -N history-incremental-search-backward-end history-search-end
-zle -N history-incremental-search-forward-end history-search-end
-bindkey '^r' history-incremental-search-backward-end
-bindkey '^s' history-incremental-search-forward-end
+autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
-bindkey '\e[A' history-beginning-search-backward-end
-bindkey '\e[B' history-beginning-search-forward-end
+bindkey "${terminfo[kcuu1]}" history-beginning-search-backward-end
+bindkey "${terminfo[kcud1]}" history-beginning-search-forward-end
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
 bindkey '^a' beginning-of-line
@@ -18,4 +28,4 @@ bindkey "^f" forward-word
 bindkey "^b" backward-word
 bindkey "${terminfo[khome]}" beginning-of-line # Fn-Left, Home
 bindkey "${terminfo[kend]}" end-of-line # Fn-Right, End
-bindkey '^[^?' backward-kill-dir
+bindkey "${terminfo[kbs]}" backward-kill-dir
