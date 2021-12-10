5fd70fa496629f732ccbae3ca217ec614a20dc7a
Michiel Roos <michiel@michielroos.com>
2021-12-10 11:35:04 +0100

    kittyMode.sh: cleanup

--- bin/kittyMode.sh
+++ bin/kittyMode.sh
@@ -2,39 +2,74 @@
 # See ~/bin/sunrise-sunset.sh
 # */5 * * * * ~/bin/kittyMode.sh
 
+declare -A kitty_background
+kitty_background=(
+  [dark]="#000000"
+  [light]="#fdf6e3"
+)
+declare -A kitty_foreground
+kitty_foreground=(
+  [dark]="#c0c0c0"
+  [light]="#586e75"
+)
+declare -A kitty_background_image
+kitty_background_image=(
+  [dark]="${HOME}/Pictures/Backgrounds/termBg.png"
+  [light]="${HOME}/Pictures/Backgrounds/termBg.day.png"
+)
+declare -A gtk_theme
+gtk_theme=(
+  [dark]="Adwaita-dark"
+  [light]="Adwaita"
+)
+declare -A bat_theme
+bat_theme=(
+  [dark]="OneHalfDark"
+  [light]="OneHalfLight"
+)
+declare -A iconlookup_dirtxt
+iconlookup_dirtxt=(
+  [dark]="67"
+  [light]="67"
+)
+declare -A iconlookup_filetxt
+iconlookup_filetxt=(
+  [dark]="238"
+  [light]="250"
+)
+
 # Needed to run from cron
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
+export PATH=/bin:/usr/bin/:/usr/local/bin
 
 nowTimestamp=$(date +%s)
-fiveMinutesAgo=$(expr $nowTimestamp - 300)
+aWhileAgo=$(expr $nowTimestamp - 300)
 recreate=false
 
 kittyThemeEnvironment="${ZDOTDIR:-${HOME}}/.config/kitty/env.conf"
-lastKittyChange=$(stat --format=%Y $kittyThemeEnvironment)
-kittyFileAge=$(expr $nowTimestamp - $lastKittyChange)
-if [[ ! -s "$kittyThemeEnvironment" || $lastKittyChange -le $fiveMinutesAgo ]]; then
-  touch $kittyThemeEnvironment
-  echo "# This file is generated automatically, do not edit by hand!" >$kittyThemeEnvironment
-  echo "# $(date)" >>$kittyThemeEnvironment
-  echo "# Edit ~/bin/kittyMode.sh instead!" >>$kittyThemeEnvironment
+if [[ ! -s "$kittyThemeEnvironment" || $(stat --format=%Y $kittyThemeEnvironment) -le $aWhileAgo ]]; then
   recreate=true
 fi
 
 zshThemeEnvironment="${ZDOTDIR:-${HOME}}/.config/zsh/themes/env.zsh"
-lastZshChange=$(stat --format=%Y $zshThemeEnvironment)
-zshFileAge=$(expr $nowTimestamp - $lastZshChange)
-if [[ ! -s "$zshThemeEnvironment" || $lastZshChange -le $fiveMinutesAgo ]]; then
-  touch $zshThemeEnvironment
-  echo "# This file is generated automatically, do not edit by hand!" >$zshThemeEnvironment
-  echo "# $(date)" >>$zshThemeEnvironment
-  echo "# Edit ~/bin/kittyMode.sh instead!" >>$zshThemeEnvironment
+if [[ ! -s "$zshThemeEnvironment" || $(stat --format=%Y $zshThemeEnvironment) -le $aWhileAgo ]]; then
   recreate=true
 fi
 
-if ($recreate); then
+if [ $recreate ] || [ "$1" != "" ]; then
   # Debug
   # notify-send 'KittyMode' "Refreshing kitty theme file" -a 'KittyMode' -i "/usr/lib/kitty/logo/kitty.png"
+
+  rm $kittyThemeEnvironment
+  echo "# This file is generated automatically, do not edit by hand!" >$kittyThemeEnvironment
+  echo "# $(date)" >>$kittyThemeEnvironment
+  echo "# Edit ~/bin/kittyMode.sh instead!" >>$kittyThemeEnvironment
+
+  rm $zshThemeEnvironment
+  echo "# This file is generated automatically, do not edit by hand!" >$zshThemeEnvironment
+  echo "# $(date)" >>$zshThemeEnvironment
+  echo "# Edit ~/bin/kittyMode.sh instead!" >>$zshThemeEnvironment
 
   location=${LOCATION:-NLXX5790}
 
@@ -55,39 +90,20 @@ if ($recreate); then
     mode=$1
   fi
 
-  if [[ $mode == "light" ]]; then
-    # Active terminals
-    kitty @ --to unix:@kitty-$(pidof kitty) set-colors -a background=\#fdf6e3 foreground=\#586e75 &
-    kitty @ --to unix:@kitty-$(pidof kitty) set-background-image ~/Pictures/Backgrounds/termBg.day.png &
+  # Active terminals
+  kitty @ --to unix:@kitty-$(pidof kitty) set-colors -a background=${kitty_background[$mode]} foreground=${kitty_foreground[$mode]}
+  kitty @ --to unix:@kitty-$(pidof kitty) set-background-image "${kitty_background_image[$mode]}"
 
-    # Gnome theme
-    gsettings set org.gnome.desktop.interface gtk-theme Adwaita &
+  # Gnome theme
+  gsettings set org.gnome.desktop.interface gtk-theme ${gtk_theme[$mode]} &
 
-    # New terminals
-    echo 'background_image ~/Pictures/Backgrounds/termBg.day.png' >>$kittyThemeEnvironment
-    echo 'background #fdf6e3' >>$kittyThemeEnvironment
-    echo 'foreground #586e75' >>$kittyThemeEnvironment
+  # New terminals
+  echo "background_image ${kitty_background_image[$mode]}" >>$kittyThemeEnvironment
+  echo "background ${kitty_background[$mode]}" >>$kittyThemeEnvironment
+  echo "foreground ${kitty_foreground[$mode]}" >>$kittyThemeEnvironment
 
-    # New zsh prompt and bat themes
-    echo 'export BAT_THEME="OneHalfLight"' >>$zshThemeEnvironment
-    echo 'export IL_C_DIRTXT=67' >>$zshThemeEnvironment
-    echo 'export IL_C_FILETXT=238' >>$zshThemeEnvironment
-  else
-    # Active terminals
-    kitty @ --to unix:@kitty-$(pidof kitty) set-colors -a background=\#000000 foreground=\#c0c0c0 &
-    kitty @ --to unix:@kitty-$(pidof kitty) set-background-image ~/Pictures/Backgrounds/termBg.png &
-
-    # Gnome theme
-    gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark &
-
-    # New terminals
-    echo 'background_image ~/Pictures/Backgrounds/termBg.png' >>$kittyThemeEnvironment
-    echo 'background #000000' >>$kittyThemeEnvironment
-    echo 'foreground #c0c0c0' >>$kittyThemeEnvironment
-
-    # New zsh prompt and bat themes
-    echo 'export BAT_THEME="OneHalfDark"' >>$zshThemeEnvironment
-    echo 'export IL_C_DIRTXT=67' >>$zshThemeEnvironment
-    echo 'export IL_C_FILETXT=250' >>$zshThemeEnvironment
-  fi
+  # New zsh prompt and bat themes
+  echo "export BAT_THEME=\"${bat_theme[$mode]}\"" >>$zshThemeEnvironment
+  echo "export IL_C_DIRTXT=\"${iconlookup_dirtxt[$mode]}\"" >>$zshThemeEnvironment
+  echo "export IL_C_FILETXT=\"${iconlookup_filetxt[$mode]}\"" >>$zshThemeEnvironment
 fi
